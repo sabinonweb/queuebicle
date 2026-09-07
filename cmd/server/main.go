6de782335664ed203929c0d4c690ebf30364c16d
sabinonweb <sabinonweb7@gmail.com>
2026-09-07 23:20:46 +0545

    [Update: Small win]

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,26 +5,53 @@ import (
   "sync"
 )
 
+type Job struct {
+  ID int
+  Value string
+}
+
+func NewJob(id int, value string) Job {
+  return Job {
+    ID: id,
+    Value: value,
+  }
+}
+
 func main() {
   var wg sync.WaitGroup
 
-  ch := make(chan int)
-  x, y, z := 1, 2, 3
+  ch := make(chan Job)
 
-  wg.Add(1)
-  go receive(ch, &wg)
+  wg.Add(5)
+  
+  for i := 0; i < 5; i++ {
+    go worker(ch, &wg, i)
+  } 
+  
+  j1 := NewJob(1, "hello")
+  j2 := NewJob(2, "World")
+  j3 := NewJob(3, "hora")
 
-  ch <- x 
-  ch <- y
-  ch <- z
+  jobs := []Job{j1, j2, j3}
+  
+  for _, j := range jobs {
+    submit_job(ch, j)
+  } 
+
   close(ch)
   wg.Wait()
 }
 
-func receive(ch chan int, wg *sync.WaitGroup) {
+func submit_job(ch chan Job, job Job) {
+  ch <- job
+}
+
+func worker(ch chan Job, wg *sync.WaitGroup, n int) {
   for v := range ch {
-    fmt.Println(v)
+    fmt.Println("From Worker", n, ":", v)
   }
 
   wg.Done()
 }
+
+
