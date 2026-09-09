1f8368de8a9b8f7f0274baeae00eb86c972ffe5a
sabinonweb <sabinonweb7@gmail.com>
2026-09-09 22:58:41 +0545

    [Update: Buffered Channel]

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,29 +20,42 @@ func NewJob(id int, value string) Job {
 func main() {
   var wg sync.WaitGroup
 
-  ch := make(chan Job)
+  ch := make(chan Job, 10)
 
   wg.Add(5)
   
   for i := 0; i < 5; i++ {
     go worker(ch, &wg, i)
-  } 
-  
+  }
+    
   j1 := NewJob(1, "hello")
   j2 := NewJob(2, "World")
   j3 := NewJob(3, "hora")
+  j4 := NewJob(4, "hello")
+  j5 := NewJob(5, "World")
+  j6 := NewJob(6, "hora")
+  j7 := NewJob(7, "hello")
+  j8 := NewJob(8, "World")
+  j9 := NewJob(9, "hora")
+  j10 := NewJob(10, "hello")
+  j11 := NewJob(11, "World")
+  j12 := NewJob(12, "hora")
 
-  jobs := []Job{j1, j2, j3}
+
+
+
+  jobs := []Job{j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12}
   
   for _, j := range jobs {
-    submit_job(ch, j)
-  } 
+    submitJob(ch, j)
+  }
+
 
   close(ch)
   wg.Wait()
 }
 
-func submit_job(ch chan Job, job Job) {
+func submitJob(ch chan Job, job Job) {
   ch <- job
 }
 
